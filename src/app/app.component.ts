1c98a030fa745f30cc6cdd687942204fa3507870
Ashutosh Sharma <ashutoshsharma0061@gmail.com>
2020-04-13 18:59:19 +0530

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,24 +9,23 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class AppComponent {
   blogForm: FormGroup;
   blogs: Array<any> = [];
-  
-  constructor(private formBuilder: FormBuilder) {
-  	this.blogs=[{'title':'A Boring Day :(','body':'By in no ecstatic wondered disposal my speaking. Direct wholly valley or uneasy it at really. Sir wish like said dull and need make. Sportsman one bed departure rapturous situation disposing his. Off say yet ample ten ought hence. Depending in newspaper an september do existence strangers. Total great saw water had mirth happy new. Projecting pianoforte no of partiality is on. Nay besides joy society him totally six. '},{'title':'Happy Life :)!!','body':'Carried nothing on am warrant towards. Polite in of in oh needed itself silent course. Assistance travelling so especially do prosperous appearance mr no celebrated. Wanted easily in my called formed suffer. Songs hoped sense as taken ye mirth at. Believe fat how six drawing pursuit minutes far. Same do seen head am part it dear open to. Whatever may scarcely judgment had. '},{'title':'Sad!!','body':'it was reall ckjsb  kjzxn l l;zcnx ol noldsbnczxl  jkldsbczio kljndlzb   l;sdnc lx .'}];
-  }
 
-  ngOnInit() {
-    this.createBlogForm();
+  constructor(private formBuilder: FormBuilder) {
+   this.blogs = [{title: 'A Boring Day :(', body: 'ample ten ought hence. ers. Total great saw water had mi'}];
+   this.blogs.push({title: 'Sad!!', body: 'it was reall ckjsb  kjzxn l l;zcnx ol noldsbnczxl  jkldsbczio kljndlzb   l;sdnc lx .'});
+   this.blogs.push({title: 'Happy Life :)!!', body: 'Carried nothing on am warrant towards. Polite in of in oh '});
+   this.createBlogForm();
   }
 
   createBlogForm() {
     this.blogForm = this.formBuilder.group({
-      'title': [''],
-      'body': ['']
+      title: [''],
+      body: ['']
     });
   }
 
   submit() {
-    this.blogs.push({'title':this.blogForm.value.title,'body':this.blogForm.value.body});
+    this.blogs.push({title: this.blogForm.value.title, body: this.blogForm.value.body});
     this.blogForm.reset();
 
   }
